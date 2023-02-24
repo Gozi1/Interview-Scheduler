5b5507ec6aa44322d62de50ce684bfe8d4b54e14
gozi1 <snwabiani@gmail.com>
2023-02-24 12:43:58 -0500

    netlify intialization

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'index.scss';
-
+import axios from 'axios';
 import Application from 'components/Application';
 
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 ReactDOM.render(<Application />, document.getElementById('root'));
